fff4fb66d34eb8760e4dd46da01ddeb8fe809a36
agung_visione <agung.p@visione-system.com>
2020-07-03 22:27:18 +0700

    Learning vue.js

---
+++ js/data.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        topTenFrameworks: [
+            { name:"Django - Python Framework", scores:100 }, 
+            { name:"Spring - Java Framework", scores:99 },
+            { name:"Laravel - PHP Framework", scores:98 },
+            { name:"Rails Framework", scores:97 },
+            { name:"React JS Framework", scores:96 },
+            { name:"Angular JS Framework", scores:95 },
+            { name:"Express Framework", scores:94 },
+            { name:"Vue.js Framework", scores:93 },
+            { name:"JQuery", scores:92 },
+            { name:"Flask - Python Framework", scores:91 }
+        ],
+    }
+})
